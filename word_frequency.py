8f52e9e4f75bfde248d55abf50e72d300700c3af
Brian Harrison <brianharrison.2002@gmail.com>
2021-08-10 11:12:52 -0400

    need to now where to place the selfs, and change the flow to how I had it when it was my code

--- word_frequency.py
+++ word_frequency.py
@@ -1,3 +1,6 @@
+from os import read, remove
+import string
+
 STOP_WORDS = [
     'a', 'an', 'and', 'are', 'as', 'at', 'be', 'by', 'for', 'from', 'has',
     'he', 'i', 'in', 'is', 'it', 'its', 'of', 'on', 'that', 'the', 'to',
@@ -7,19 +10,27 @@ STOP_WORDS = [
 
 class FileReader:
     def __init__(self, filename):
-        pass
-
+        self.filename = filename
+        
     def read_contents(self):
         """
         This should read all the contents of the file
         and return them as one string.
         """
-        raise NotImplementedError("FileReader.read_contents")
-
+        file = open(self.filename)
+        file.readline()
+        file = str(self).split(" ")
+        print(file)
+        return file 
+        
+      
 
 class WordList:
     def __init__(self, text):
+        
+        self.text = file
         pass
+
 
     def extract_words(self):
         """
@@ -27,14 +38,38 @@ class WordList:
         is responsible for lowercasing all words and stripping
         them of punctuation.
         """
-        raise NotImplementedError("WordList.extract_words")
+        
+        punctuation = ['.' , ',' , ':', "'" , '"']
+        for occurance in punctuation:
+            file = str(self.text.remove(occurance,""))
+        return file
+        pass
+
+   
+    
 
     def remove_stop_words(self):
         """
         Removes all stop words from our word list. Expected to
         be run after extract_words.
         """
-        raise NotImplementedError("WordList.remove_stop_words")
+        pass
+        STOP_WORDS = [
+     '—', ' an ', ' and ', ' are ', ' as ', ' at ', ' be ', ' by ', ' for ', ' from ', ' has ', ' he ',
+    ' i ', ' in ', ' is ', ' it ', ' its ', ' of ', ' on ', ' that ', ' the ', ' to ', ' were ',
+     ' will ' , ' with ' , ' a ' ]
+        for word in STOP_WORDS:
+            file = str(self.text).replace(word," ")
+        return file
+    
+ 
+
+    
+    
+        
+    
+    
+
 
     def get_freqs(self):
         """
@@ -43,12 +78,17 @@ class WordList:
         extract_words and remove_stop_words. The data structure
         could be a dictionary or another type of object.
         """
-        raise NotImplementedError("WordList.get_freqs")
+        wordcount = {};
+        for single_word in file: 
+        wordcount[single_word] = wordcount.get(single_word, 0) + 1
+        
+        
+        pass
 
 
 class FreqPrinter:
     def __init__(self, freqs):
-        pass
+        self.freqs = freqs
 
     def print_freqs(self):
         """
@@ -67,7 +107,13 @@ class FreqPrinter:
        rights | 6    ******
         right | 6    ******
         """
-        raise NotImplementedError("FreqPrinter.print_freqs")
+        pass
+        wordcount = {};
+        for single_word in file: 
+            wordcount[single_word] = wordcount.get(single_word, 0) + 1
+        sorted_wordcount = sorted(wordcount.items(), key=lambda seq: seq[1], reverse=True)
+        for x in sorted_wordcount:
+            print(x[0], x[1], "*"*(x[1]))
 
 
 if __name__ == "__main__":
@@ -83,7 +129,9 @@ if __name__ == "__main__":
     file = Path(args.file)
     if file.is_file():
         reader = FileReader(file)
+        print(FileReader(file))
         word_list = WordList(reader.read_contents())
+        print(word_list)
         word_list.extract_words()
         word_list.remove_stop_words()
         printer = FreqPrinter(word_list.get_freqs())
